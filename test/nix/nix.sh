63b26490476212e9767bb447593a04322518b87b
gbhu753 <gbhu753@aucklanduni.ac.nz>
2025-12-14 13:05:44 +0000

    Chore: Add specific shell scriptr

---
+++ test/nix/nix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This test file will be executed against the 'nix' scenario
+# which installs Nix without a specific flake
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "nix is installed" bash -c "command -v nix"
+
+check "nix version" bash -c "nix --version"
+
+check "nix.conf exists" bash -c "test -f /etc/nix/nix.conf"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
