aa7ea213d3e89e381655dcaf603b06aaf076a40d
gbhu753 <gbhu753@aucklanduni.ac.nz>
2025-12-15 12:53:35 +0000

    Chore: Move test to use fastfetch

--- test/nix/flake.sh
+++ test/nix/flake.sh
@@ -8,17 +8,9 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Debug: output the result of the command
-echo "DEBUG: Testing 'zsh -c \"command -v nix\"'"
-zsh -c "command -v nix" || echo "DEBUG: Command failed or nix not found"
-
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "nix is installed" bash -c "command -v nix"
-
-check "nix version" bash -c "nix --version"
-
-check "nix.conf exists" bash -c "test -f /etc/nix/nix.conf"
+check "fastfetch is installed" bash -c "command -v fastfetch"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
